4302616e37275af3b4901ffab10462ef9398d587
Csekiro <csekiro@outlook.com>
2026-03-30 15:24:52 +0800

    init

---
+++ src/client/java/Csekiro/client/BetterteamClient.java
@@ -0,0 +1,10 @@
+package Csekiro.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterteamClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/Csekiro/Betterteam.java
@@ -0,0 +1,10 @@
+package Csekiro;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Betterteam implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
